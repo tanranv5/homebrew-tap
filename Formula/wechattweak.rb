29e3da2f73daeb0b16532914c2666a3b17d2a318
renjiangtao <trweb@vip.qq.com>
2026-09-17 14:00:19 +0800

    Bump wechattweak formula to v270098

--- Formula/wechattweak.rb
+++ Formula/wechattweak.rb
@@ -1,10 +1,10 @@
 class Wechattweak < Formula
   desc "A command-line tool for tweaking WeChat"
   homepage "https://github.com/tanranv5/WeChatTweak"
-  url "https://github.com/tanranv5/WeChatTweak/releases/download/v36559.2/wechattweak-v36559.2-macos-universal.tar.gz"
-  sha256 "f95637ebab51039cd5cff138b9b239c4fb3a4902298608d658eaa12d2d48c3e0"
+  url "https://github.com/tanranv5/WeChatTweak/releases/download/v270098/wechattweak-v270098-macos-universal.tar.gz"
+  sha256 "7aff6201bb07f2c9be75f461ab86e604a6ab6d9626d59d65deb9e544c6846f47"
   license "AGPL-3.0"
-  version "36559.2"
+  version "270098"
 
   def install
     bin.install "wechattweak"
